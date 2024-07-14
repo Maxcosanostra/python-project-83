251db78d856672bfc4e00e8d6d1d80672779e8c5
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2024-07-15 03:42:24 +0500

    bugs fixed

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -100,8 +100,7 @@ def view_url(id):
     cursor = conn.cursor()
     cursor.execute("SELECT id, name, TO_CHAR(created_at, 'YYYY-MM-DD') as created_at FROM urls WHERE id = %s;", (id,))
     url = cursor.fetchone()
-    cursor.execute("SELECT id, status_code, h1, title, description, TO_CHAR(created_at, 'YYYY-MM-DD') as created_at FROM url_checks WHERE url_id = %s ORDER BY created_at DESC;", 
-(id,))
+    cursor.execute("SELECT id, status_code, h1, title, description, TO_CHAR(created_at, 'YYYY-MM-DD') as created_at FROM url_checks WHERE url_id = %s ORDER BY created_at DESC;", (id,))
     checks = cursor.fetchall()
     cursor.close()
     conn.close()
